179ef801caeb7bff913553132c14050c891f239f
JedienlaPasta <sebastian1377@gmail.com>
2026-04-12 23:32:36 -0400

    Agregadas todas las rutas principales del portal admin

---
+++ app/(admin)/admin/propietarios/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PropietariosPage() {
+  return (
+    <div className="flex h-full flex-col space-y-8 bg-gray-50/50 p-6 lg:p-8">
+      Propietarios
+    </div>
+  );
+}
